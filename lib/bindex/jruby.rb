5b751bc0f3c487f06a605ba145e91fba18ad57fb
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-12-20 14:31:06 +0200

    Remove a useless semicolon in jruby.rb
    Too much Java, lol :)

--- lib/bindex/jruby.rb
+++ lib/bindex/jruby.rb
@@ -2,7 +2,7 @@ require 'bindex/jruby_internals'
 
 java_import com.gsamokovarov.bindex.SetExceptionBindingsEventHook
 
-JRuby.runtime.add_event_hook(SetExceptionBindingsEventHook.new);
+JRuby.runtime.add_event_hook(SetExceptionBindingsEventHook.new)
 
 ::Exception.class_eval do
   def bindings
