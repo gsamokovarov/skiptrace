485238204597e51757c76a99956efc737621205e
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-12-21 01:15:06 +0200

    Fix Ruby 2.1.0-preview compile detection

--- ext/bindex/extconf.rb
+++ ext/bindex/extconf.rb
@@ -1,13 +1,13 @@
 require "mkmf"
 
-case RUBY_VERSION
+case "#{RUBY_VERSION}#{RUBY_PATCHLEVEL}"
 when /1\.9\.3/
   $CFLAGS   << " -D RUBY_193"
   $INCFLAGS << " -I./ruby_193/"
 when /2\.0\.*/
   $CFLAGS   << " -D RUBY_20"
   $INCFLAGS << " -I./ruby_20/"
-when /2\.1\.*preview\*/
+when "2.1.0-1"
   $CFLAGS   << " -D RUBY_21preview"
   $INCFLAGS << " -I./ruby_21preview/"
 when /2\.1\.*/
