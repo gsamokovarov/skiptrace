6de03c31c82c73c7287270904eee4958355d63ba
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-12-20 14:55:24 +0200

    Silence warnings on JRuby
    While I cannot be sure that the bindings collection will work properly
    outside of the non-JITted JRuby modes (because of the optimizations
    JRuby does, local variables may end up on the stack and changing them in
    a binding may lead to unexpected results), lets document that and not
    warn every time. The test suite runs fine even without --debug or --dev
    or interpreted mode. This can change, though.

--- lib/bindex/jruby.rb
+++ lib/bindex/jruby.rb
@@ -2,7 +2,12 @@ require 'bindex/jruby_internals'
 
 java_import com.gsamokovarov.bindex.SetExceptionBindingsEventHook
 
-JRuby.runtime.add_event_hook(SetExceptionBindingsEventHook.new)
+begin
+  previous_verbose, $VERBOSE = $VERBOSE, nil
+  JRuby.runtime.add_event_hook(SetExceptionBindingsEventHook.new)
+ensure
+  $VERBOSE = previous_verbose
+end
 
 ::Exception.class_eval do
   def bindings
