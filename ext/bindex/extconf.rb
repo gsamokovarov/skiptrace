57526cd8e853dc17a0bc72c7cd658663e85b489a
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-12-22 10:29:55 +0200

    Compile only on CRuby and don't fail on other rubies
    JRuby has the JARs included and everything is in plain Ruby for
    Rubinius.

--- ext/bindex/extconf.rb
+++ ext/bindex/extconf.rb
@@ -1,20 +1,29 @@
-require "mkmf"
+case RUBY_ENGINE
+when "ruby"
+  require "mkmf"
 
-case "#{RUBY_VERSION}#{RUBY_PATCHLEVEL}"
-when /1\.9\.3/
-  $CFLAGS   << " -D RUBY_193"
-  $INCFLAGS << " -I./ruby_193/"
-when /2\.0\.*/
-  $CFLAGS   << " -D RUBY_20"
-  $INCFLAGS << " -I./ruby_20/"
-when "2.1.0-1"
-  $CFLAGS   << " -D RUBY_21preview"
-  $INCFLAGS << " -I./ruby_21preview/"
-when /2\.1\.*/
-  $CFLAGS   << " -D RUBY_21"
-  $INCFLAGS << " -I./ruby_21/"
+  case "#{RUBY_VERSION}#{RUBY_PATCHLEVEL}"
+  when /1\.9\.3/
+    $CFLAGS   << " -D RUBY_193"
+    $INCFLAGS << " -I./ruby_193/"
+  when /2\.0\.*/
+    $CFLAGS   << " -D RUBY_20"
+    $INCFLAGS << " -I./ruby_20/"
+  when "2.1.0-1"
+    $CFLAGS   << " -D RUBY_21preview"
+    $INCFLAGS << " -I./ruby_21preview/"
+  when /2\.1\.*/
+    $CFLAGS   << " -D RUBY_21"
+    $INCFLAGS << " -I./ruby_21/"
+  end
+
+  $CFLAGS << " -g3 -O0" if ENV["DEBUG"]
+
+  create_makefile("bindex/cruby")
+else
+  IO.write(File.expand_path("../Makefile", __FILE__), <<-END)
+    all install static install-so install-rb: Makefile
+    .PHONY: all install static install-so install-rb
+    .PHONY: clean clean-so clean-static clean-rb
+  END
 end
-
-$CFLAGS << " -g3 -O0" if ENV["DEBUG"]
-
-create_makefile("bindex/cruby")
