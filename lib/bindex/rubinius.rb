6e6e8ab70d05b082ddecf6afd4a26a510367f0a1
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-12-22 17:18:07 +0200

    Rubinius: Filter out backtrace locations with nil variables
    We can't create bindings out of those frames.

--- lib/bindex/rubinius.rb
+++ lib/bindex/rubinius.rb
@@ -19,13 +19,18 @@ module Bindex
 
     # Filters internal Rubinius locations.
     #
-    # The reason for this is that some methods, like ::Kernel.raise, are
-    # implemented in Ruby for Rubinius. Therefore, we will get a binding for
-    # them as well.
+    # There are a couple of reasons why we wanna filter out the locations.
     #
-    # To align the current_bindings implementation with the CRuby one, where
-    # the first binding will usually be binding of caller, we can use this
-    # object to clean bindings for internal Rubinius methods.
+    # * ::Kernel.raise, is implemented in Ruby for Rubinius. We don't wanna
+    #   have the frame for it to align with the CRuby and JRuby implementations.
+    #
+    # * For internal methods location variables can be nil. We can't create a
+    #   bindings for them.
+    #
+    # * Bindings from the current file are considered internal and ignored.
+    #
+    # We do that all that so we can align the bindings with the backtraces
+    # entries.
     class InternalLocationFilter
       def initialize(locations)
         @locations = locations
@@ -33,7 +38,9 @@ module Bindex
 
       def filter
         @locations.reject do |location|
-          location.file.start_with?('kernel/') || location.file == __FILE__
+          location.file.start_with?('kernel/delta/kernel.rb') ||
+            location.file == __FILE__ ||
+            location.variables.nil?
         end
       end
     end
