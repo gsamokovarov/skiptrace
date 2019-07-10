ee9251e67a455dcacf7ec929044373953f9207dc
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-07-10 16:25:29 +0300

    Dump the CRuby lib in skiptrace/internal/cruby

--- ext/skiptrace/extconf.rb
+++ ext/skiptrace/extconf.rb
@@ -5,7 +5,7 @@ when "ruby"
   $CFLAGS << " -Wall"
   $CFLAGS << " -g3 -O0" if ENV["DEBUG"]
 
-  create_makefile("skiptrace/cruby")
+  create_makefile("skiptrace/internal/cruby")
 else
   IO.write(File.expand_path("../Makefile", __FILE__), <<-END)
     all install static install-so install-rb: Makefile
